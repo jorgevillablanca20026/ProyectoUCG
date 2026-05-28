d4300776778c65274797c63e39a96fba9690497f
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-27 23:01:46 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,36 +1,31 @@
 import pandas as pd
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = "."
 
 USERS_FILE = os.path.join(BASE_DIR, "users1.csv")
 DATA_FILE = os.path.join(BASE_DIR, "data1.csv")
 
 
-# ================= USERS =================
 def load_users():
-    try:
-        return pd.read_csv(USERS_FILE, encoding="utf-8")
-    except:
+    if not os.path.exists(USERS_FILE):
         df = pd.DataFrame(columns=["usuario", "password"])
-        df.to_csv(USERS_FILE, index=False, encoding="utf-8")
+        df.to_csv(USERS_FILE, index=False)
         return df
+    return pd.read_csv(USERS_FILE)
 
 
 def save_users(df):
-    # 🔥 esto garantiza escritura real
-    df.to_csv(USERS_FILE, index=False, encoding="utf-8")
+    df.to_csv(USERS_FILE, index=False)
 
 
-# ================= PRODUCTS =================
 def load_data():
-    try:
-        return pd.read_csv(DATA_FILE, encoding="utf-8")
-    except:
+    if not os.path.exists(DATA_FILE):
         df = pd.DataFrame(columns=["id","nombre","descripcion","precio","stock","categoria"])
-        df.to_csv(DATA_FILE, index=False, encoding="utf-8")
+        df.to_csv(DATA_FILE, index=False)
         return df
+    return pd.read_csv(DATA_FILE)
 
 
 def save_data(df):
-    df.to_csv(DATA_FILE, index=False, encoding="utf-8")
+    df.to_csv(DATA_FILE, index=False)
