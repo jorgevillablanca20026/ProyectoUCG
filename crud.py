d7b17849af65e5c173a108692f8e8006ef938574
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-27 22:17:08 -0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,22 +1,22 @@
-from data import load_data, save_data
 import pandas as pd
+from data import load_data, save_data
 
 
-def create_product(producto):
+def create_product(p):
     df = load_data()
-    df = pd.concat([df, pd.DataFrame([producto])], ignore_index=True)
+    df = pd.concat([df, pd.DataFrame([p])], ignore_index=True)
     save_data(df)
 
 
-def delete_product(id_producto):
+def delete_product(id_):
     df = load_data()
-    df = df[df["id"] != id_producto]
+    df = df[df["id"] != id_]
     save_data(df)
 
 
-def update_stock(id_producto, stock):
+def update_stock(id_, stock):
     df = load_data()
-    df.loc[df["id"] == id_producto, "stock"] = stock
+    df.loc[df["id"] == id_, "stock"] = stock
     save_data(df)
 
 
