5a141836303e513d81811aef73e8c166407ef4d9
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-27 22:24:33 -0500

    Refactor data loading functions to check file existence

--- data.py
+++ data.py
@@ -8,24 +8,26 @@ USERS_FILE = os.path.join(BASE_DIR, "users.csv")
 
 
 def load_data():
-    try:
-        return pd.read_csv(DATA_FILE, encoding="utf-8")
-    except:
-        return pd.DataFrame(columns=[
-            "id", "nombre", "descripcion", "precio", "stock", "categoria"
-        ])
+    if not os.path.exists(DATA_FILE):
+        df = pd.DataFrame(columns=["id","nombre","descripcion","precio","stock","categoria"])
+        df.to_csv(DATA_FILE, index=False)
+        return df
+
+    return pd.read_csv(DATA_FILE)
 
 
 def save_data(df):
-    df.to_csv(DATA_FILE, index=False, encoding="utf-8")
+    df.to_csv(DATA_FILE, index=False)
 
 
 def load_users():
-    try:
-        return pd.read_csv(USERS_FILE, encoding="utf-8")
-    except:
-        return pd.DataFrame(columns=["usuario", "password"])
+    if not os.path.exists(USERS_FILE):
+        df = pd.DataFrame(columns=["usuario","password"])
+        df.to_csv(USERS_FILE, index=False)
+        return df
+
+    return pd.read_csv(USERS_FILE)
 
 
 def save_users(df):
-    df.to_csv(USERS_FILE, index=False, encoding="utf-8")
+    df.to_csv(USERS_FILE, index=False)
