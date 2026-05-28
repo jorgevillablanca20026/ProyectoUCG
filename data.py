a49ef16635264ef654a086ccd69389220447872f
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-27 22:01:10 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import os
 
 FILE = "data.csv"
 
-
 def load_data():
+    if not os.path.exists(FILE):
+        df = pd.DataFrame(columns=[
+            "id", "nombre", "descripcion", "precio", "stock", "categoria"
+        ])
+        df.to_csv(FILE, index=False)
+        return df
+
     return pd.read_csv(FILE)
 
 
