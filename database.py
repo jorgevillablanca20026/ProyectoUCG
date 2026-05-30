c78fe0b581beb4966077b43b04c124d432a0664d
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-30 09:23:41 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 from supabase import create_client
 import streamlit as st
 
+
 def get_db():
     return create_client(
         st.secrets["SUPABASE_URL"],
         st.secrets["SUPABASE_KEY"]
     )
-
-def init_db():
-    pass
