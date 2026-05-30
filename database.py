abee3f42c77d37ba12810695bab9e6edb6d56f7e
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-30 09:03:00 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,32 +1,11 @@
-import sqlite3
+from supabase import create_client
+import streamlit as st
 
-DB = "app.db"
-
-def get_conn():
-    return sqlite3.connect(DB)
+def get_db():
+    return create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
 
 def init_db():
-    conn = get_conn()
-    c = conn.cursor()
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        usuario TEXT UNIQUE,
-        password TEXT
-    )
-    """)
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT,
-        descripcion TEXT,
-        precio REAL,
-        stock INTEGER,
-        categoria TEXT
-    )
-    """)
-
-    conn.commit()
-    conn.close()
+    pass
